b09253627aa0d8f37a71ffe8b3395b5c0dd2c253
SHU <maroran@gmail.com>
2023-04-07 20:10:17 +0900

    FIX

--- gulp/gulp-wordpress/public_html/functions.php
+++ gulp/gulp-wordpress/public_html/functions.php
@@ -14,7 +14,9 @@ add_filter('register_post_type_args', 'post_has_archive', 10, 2);
 // scriptとcssを読み込む
 function my_theme_scripts()
 {
-	wp_enqueue_style('pc', get_theme_file_uri('./assets/css/style.css','1.0'));
+	
+	wp_enqueue_style('reset', get_theme_file_uri('./assets/css/destyle.min.css','1.0'));
+	wp_enqueue_style('pc', get_theme_file_uri('./assets/css/style.css',['reset'],'1.0'));
 	wp_enqueue_script('allpage', get_theme_file_uri('./assets/js/script.js','1.0'));
 }
 add_action('wp_enqueue_scripts', 'my_theme_scripts');
